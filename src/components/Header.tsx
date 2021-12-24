311d2b42f7a45e882f5d9b0e1424f61137605723
jcruz375 <joaocruz375@gmail.com>
2021-12-23 22:10:27 -0300

    header correction

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,38 @@
-import { Flex, Image } from "@chakra-ui/react";
+import { Flex, Icon, Image, Grid } from "@chakra-ui/react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { BiArrowBack } from "react-icons/bi";
 
 export function Header() {
+  const { asPath } = useRouter();
+  const isHomePage = asPath === '/'
   return (
     <Flex
       as="header"
-      w={{base:'600px', md:'24px'}}
-      height={{base:'400px', md:'24px'}}
-      justifyContent="center"
+      width={[800, 1240, 1800]}
+      height={[126, 100]}
       align="center"
+      justify="center"
       p={["10", "8"]}
     >
-      <Image src="Logo.svg" alt="Worldtrip" />
+      {!isHomePage && (
+        <Grid
+          mx="auto"
+          alignItems="center"
+          templateColumns="repeat(3, 1fr)"
+          justifyContent="center"
+        >
+          <Link href="/">
+            <a>
+              <Icon as={BiArrowBack} />
+            </a>
+          </Link>
+          <Image src="Logo.png" alt="Worldtrip" />
+        </Grid>
+      )}
+      {isHomePage && (
+        <Image src="Logo.png" alt="Worldtrip" />
+      )}
     </Flex>
   );
 }
